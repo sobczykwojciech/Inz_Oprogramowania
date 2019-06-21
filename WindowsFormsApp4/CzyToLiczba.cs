c5a15d9fc6585eb648a6c181376ec4d4030e6000
oKub4n <lysiak.jakub@wp.pl>
2019-06-21 11:58:48 +0200

    Create CzyToLiczba.cs
    Dodanie Klasy CzyToLiczba.cs

---
+++ WindowsFormsApp4/CzyToLiczba.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using Program1;
+
+namespace Program1
+{
+    internal class CzyLiczba
+    {
+        public static bool CzyOK(string X, int Z)
+        {
+            bool Q = false;
+
+            if (X == "")
+            {
+                MessageBox.Show("Brak liczby");  
+            }
+            else
+            {
+                if (Z == 1)
+                {
+                    Q = true;
+                    try
+                    {
+                        double dane_ok = System.Convert.ToDouble(X);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nieprawidłowe znaki");
+                        Q = false;
+                    }
+                }
+                else
+                {
+                    Q = true;
+                    try
+                    {
+                        double dane_ok = System.Convert.ToInt32(X);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Nieprawidłowe znaki");
+                        Q = false;
+
+                    }
+                } 
+            }
+            return Q;
+        }
+    }
+}            
