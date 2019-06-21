2eedd583f10140e4cd89392ddee9d979960d7c7c
oKub4n <lysiak.jakub@wp.pl>
2019-06-21 11:59:07 +0200

    Create Export.cs
    Dodanie Klasy Export.cs

---
+++ WindowsFormsApp4/Export.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Excel = Microsoft.Office.Interop.Excel;
+using Program1;
+using System.Globalization;
+using System.Data.OleDb;
+using System.IO;
+
+namespace WindowsFormsApp4
+{
+    public class ExportDoExcel
+
+    // na podstawie: https://stackoverflow.com/questions/18182029/how-to-export-datagridview-data-instantly-to-excel-on-button-click
+
+    {
+        public static void ExportToExcel(DataGridView A)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "Harmonogram.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Copy DataGridView results to clipboard
+                copyAlltoClipboard(A);
+
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Application xlexcel = new Excel.Application();
+
+                xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
+                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                // Format column D as text before pasting results, this was required for my data
+                //Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
+                //rng.NumberFormat = "@";
+
+                xlWorkSheet.Cells[1, 2] = "Nr";
+                xlWorkSheet.Cells[1, 3] = "Data Płatności";
+                xlWorkSheet.Cells[1, 4] = "Saldo";
+                xlWorkSheet.Cells[1, 5] = "Kapitał";
+                xlWorkSheet.Cells[1, 6] = "Odsetki";
+                xlWorkSheet.Cells[1, 7] = "Rata";
+
+                // Paste clipboard results to worksheet range
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[2, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                // For some reason column A is always blank in the worksheet. ¯\_(ツ)_/¯
+                // Delete blank column A and select cell A1
+                Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                delRng.Delete(Type.Missing);
+                xlWorkSheet.get_Range("A1").Select();
+                
+
+                // Save the excel file under the captured location from the SaveFileDialog
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlexcel.DisplayAlerts = true;
+                xlWorkBook.Close(true, misValue, misValue);
+                xlexcel.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlexcel);
+
+                // Clear Clipboard and DataGridView selection
+                Clipboard.Clear();
+                A.ClearSelection();
+
+                // Open the newly saved excel file
+                if (File.Exists(sfd.FileName))
+                    System.Diagnostics.Process.Start(sfd.FileName);
+            }
+        }
+
+        private static void copyAlltoClipboard(DataGridView A)
+        {
+            A.SelectAll();
+            DataObject dataObj = A.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+
+    public class ImportZExcel
+
+    // na podstawie: 
+
+    {
+        public static void ImportFromExcel(DataGridView A)
+        {
+
+            string Text1 = string.Empty;
+            string Text2 = "Arkusz1";
+
+            {
+                OpenFileDialog openfile1 = new OpenFileDialog();
+                if (openfile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Text1 = openfile1.FileName;
+                }
+                {
+                    string pathconn = "Provider = Microsoft.jet.OLEDB.4.0; Data source=" + Text1 + ";Extended Properties=\"Excel 8.0;HDR= yes;\";"; // do xlsx potrzebny jest OLEDE.12.0 który nie działa
+                    OleDbConnection conn = new OleDbConnection(pathconn);
+                    OleDbDataAdapter MyDataAdapter = new OleDbDataAdapter("Select * from [" + Text2 + "$]", conn);
+                    System.Data.DataTable dt = new System.Data.DataTable();
+
+                    MyDataAdapter.Fill(dt);
+                    A.DataSource = dt;
+                }
+            }
+        }
+    }
+}
+  
+
